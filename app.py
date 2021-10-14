935d2390f64df837a35f397745e8f4e0cabd521c
Georg Popp <Georg.Popp@sovanta.com>
2021-10-14 16:52:19 +0200

    Output embellishments

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def app():
     include_symbols = convert_validation_to_bool(check_input(default_pattern, "Do you want to include symbols? (e.g. @#$%!) [Y/n]: ", default_error_message))
     include_numbers = convert_validation_to_bool(check_input(default_pattern, "Do you want to include numbers? (e.g. 123456) [Y/n]: ", default_error_message))
     include_lowercase_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to include lowercase characters? (e.g. abcdefg) [Y/n]: ", default_error_message))
-    include_uppercase_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to include uppercase characters? (e.g. ABCDEFG) [Y/n]: ", default_error_message))
+    include_uppercase_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to include UPPERCASE characters? (e.g. ABCDEFG) [Y/n]: ", default_error_message))
     exclude_similar_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to exclude similar characters? (e.g. i, l, I, 1, L, o, O, 0) [Y/n]: ", default_error_message))
 
-    print(generate_password(length_of_password, include_symbols, include_numbers, include_lowercase_characters, include_uppercase_characters, exclude_similar_characters))
+    print("Your password: \n" + generate_password(length_of_password, include_symbols, include_numbers, include_lowercase_characters, include_uppercase_characters, exclude_similar_characters))
 
 app()
