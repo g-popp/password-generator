2c04810fd3307f27a38a4e6215741d46a51269b6
Georg Popp <Georg.Popp@sovanta.com>
2021-10-14 15:22:59 +0200

    Fix: Remove default message

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from utils.check_input import check_input
 def app():
     # Defaults: Pattern, Message, Error Message
     default_pattern = "^(Y|n)$"
-    default_message = "Do you want to include symbols? (e.g. @#$%!) [Y/n]: "
     default_error_message = "Error! Make sure you only use 'Y' for 'YES' and 'n' for 'No'"
 
     print("---Generate a random password---")
@@ -20,11 +19,11 @@ def app():
         else:
             break
 
-    include_symbols = check_input(default_pattern, default_message, default_error_message)
-    include_numbers = check_input(default_pattern, default_message, default_error_message)
-    include_lowercase_characters = check_input(default_pattern, default_message, default_error_message)
-    include_uppercase_characters = check_input(default_pattern, default_message, default_error_message)
-    exclude_similar_characters = check_input(default_pattern, default_message, default_error_message)
+    include_symbols = check_input(default_pattern, "Do you want to include symbols? (e.g. @#$%!) [Y/n]: ", default_error_message)
+    include_numbers = check_input(default_pattern, "Do you want to include numbers? (e.g. 123456) [Y/n]: ", default_error_message)
+    include_lowercase_characters = check_input(default_pattern, "Do you want to include lowercase characters? (e.g. abcdefg) [Y/n]: ", default_error_message)
+    include_uppercase_characters = check_input(default_pattern, "Do you want to include uppercase characters? (e.g. ABCDEFG) [Y/n]: ", default_error_message)
+    exclude_similar_characters = check_input(default_pattern, "Do you want to exclude similar characters? (e.g. i, l, I, 1, L, o, O, 0) [Y/n]: ", default_error_message)
 
     generate_password(length_of_password, include_symbols, include_numbers, include_lowercase_characters, include_uppercase_characters, exclude_similar_characters)
 
