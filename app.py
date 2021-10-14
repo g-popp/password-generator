d10661f6425814be22c64864fd970cc5316f9e9c
Georg Popp <Georg.Popp@sovanta.com>
2021-10-14 16:41:57 +0200

    Print Password

--- app.py
+++ app.py
@@ -20,12 +20,13 @@ def app():
         else:
             break
 
+    # Validate other input parameters
     include_symbols = convert_validation_to_bool(check_input(default_pattern, "Do you want to include symbols? (e.g. @#$%!) [Y/n]: ", default_error_message))
     include_numbers = convert_validation_to_bool(check_input(default_pattern, "Do you want to include numbers? (e.g. 123456) [Y/n]: ", default_error_message))
     include_lowercase_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to include lowercase characters? (e.g. abcdefg) [Y/n]: ", default_error_message))
     include_uppercase_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to include uppercase characters? (e.g. ABCDEFG) [Y/n]: ", default_error_message))
     exclude_similar_characters = convert_validation_to_bool(check_input(default_pattern, "Do you want to exclude similar characters? (e.g. i, l, I, 1, L, o, O, 0) [Y/n]: ", default_error_message))
 
-    generate_password(length_of_password, include_symbols, include_numbers, include_lowercase_characters, include_uppercase_characters, exclude_similar_characters)
+    print(generate_password(length_of_password, include_symbols, include_numbers, include_lowercase_characters, include_uppercase_characters, exclude_similar_characters))
 
 app()
